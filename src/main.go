1e1db8b5f5e079ba86b084c92ebd0f2e8e8aa8cc
alex-worker <alex-worker@email>
2019-04-08 19:46:29 +0300

    refactoring

--- src/main.go
+++ src/main.go
@@ -2,15 +2,36 @@ package main
 
 import (
     "fmt"
-    "./entity"
+    "./myengine"
 )
 
 // this is a comment
 
-var myEntity entity.BasicEntity
+// type person struct{
+    // name string
+    // age int
+// }
 
-type myScene struct {}
+// var myEntity = myengine.BasicEntity{ Id: 0, Parent: nil, Children: nil }
+
+// type Scene struct {}
+// func (*Scene) Type() string { return "myGame" }
+// func (*Scene) Preload() int { return 600 }
+// 
+// var myScene Scene
 
 func main() {
+    
+    var myEntity = myengine.NewBasic()
+
+    // var tom = person{ name: "lol", age: 40 }
+    // fmt.Println( myScene.Preload() )
+    var ent_id uint64 = myEntity.ID()
+    // fmt.Println( myEntity.ID )
+    fmt.Println( ent_id )
+
+    // fmt.Println( tom.name )
+    // fmt.Println( tom.age )
+    
     fmt.Println("Hello-World")
 }
