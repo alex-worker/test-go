8d5165abd6fb4a44156e341e3e2d402e9f635a60
alex-worker <alex-worker@email>
2019-04-09 23:41:47 +0300

    грузим карту при помощи диких плясок с бубнами

--- src/main.go
+++ src/main.go
@@ -8,22 +8,20 @@ import (
     "./ui"
 )
 
-// var level *game.Level
-var scene world.Scene
+var scene *world.Scene
 
 func main(){
 
     fmt.Println("Hello!")
-    // level = game.LoadLevelFromFile("game/maps/level1.map")
-    scene = world.LoadScene("game/maps/laboratory3.tmx")    
+    scene = world.LoadScene("game/maps/laboratory3.tmx")
 
-    // fmt.Println( myMap[:1] )
+    // fmt.Println( scene.Map )
 
     ui.Init()
 
     for true {
         ui.Draw( scene )
-        if !ui.Update() {
+        if !ui.Update( ) {
 			break
 		}
     }
