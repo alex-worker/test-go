1e421103a05a8d678a9634f49c916066d847a3aa
alex-worker <alex-worker@email>
2019-04-09 11:47:16 +0300

    load map

---
+++ src/game/game.go
@@ -0,0 +1,32 @@
+package game
+
+import (
+	"fmt"
+	"bufio"
+	"os"
+)
+
+type Tile rune
+const (
+	StoneWall Tile = '#'
+	DirtFloof Tile = '.'
+	Door Tile = '|'
+)
+
+type Level struct {
+	Map[][]Tile
+}
+
+func LoadLevelFromFile(filename string) *Level {
+	file, err := os.Open(filename)
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	scanner :=  bufio.NewScanner(file)
+	for ( scanner.Scan() ){
+		fmt.Println(scanner.Text())
+	}
+	return nil
+}
