709de27378eb3f31c32657f33a3eb476ad037293
alex-worker <alex-worker@email>
2019-04-11 01:08:11 +0300

    add game events

---
+++ src/engine/def/events.go
@@ -0,0 +1,27 @@
+package def
+
+// GameEvent действия
+type GameEvent uint8
+const (
+
+	// EventEvent нет событий
+	EventNo GameEvent = iota
+
+	// EventPressUp вверх
+	EventPressUp
+	// EventPressDown вниз
+	EventPressDown
+	// EventPressLeft влево
+	EventPressLeft
+	// EventPressRight вправо
+	EventPressRight
+	// EventPressFire огонь
+	EventPressFire
+	
+	// EventPressRestart рестарт
+	EventPressRestart
+
+	// EventQuit завершение 
+	EventQuit
+
+)
