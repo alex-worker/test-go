dd89ddcc2e6c2fb854008c9f6212060b2e3838e5
alex-worker <43813540+alex-worker@users.noreply.github.com>
2019-04-11 22:50:16 +0300

    добавлена анимация ьайлов

--- src/data/castletiles.tsx
+++ src/data/castletiles.tsx
@@ -1,5 +1,107 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset name="castletiles" tilewidth="32" tileheight="32" tilecount="24320" columns="64">
+<tileset name="castletiles" tilewidth="32" tileheight="32" tilecount="6080" columns="64">
  <image source="tiles.png" trans="ff00ff" width="2048" height="3040"/>
+ <tile id="34">
+  <animation>
+   <frame tileid="34" duration="100"/>
+   <frame tileid="35" duration="100"/>
+   <frame tileid="36" duration="100"/>
+   <frame tileid="37" duration="100"/>
+   <frame tileid="38" duration="100"/>
+   <frame tileid="39" duration="100"/>
+   <frame tileid="40" duration="100"/>
+   <frame tileid="41" duration="100"/>
+   <frame tileid="42" duration="100"/>
+   <frame tileid="43" duration="100"/>
+   <frame tileid="44" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="47">
+  <animation>
+   <frame tileid="47" duration="100"/>
+   <frame tileid="48" duration="100"/>
+   <frame tileid="49" duration="100"/>
+   <frame tileid="50" duration="100"/>
+   <frame tileid="51" duration="100"/>
+   <frame tileid="52" duration="100"/>
+   <frame tileid="53" duration="100"/>
+   <frame tileid="54" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="64">
+  <animation>
+   <frame tileid="64" duration="100"/>
+   <frame tileid="65" duration="100"/>
+   <frame tileid="66" duration="100"/>
+   <frame tileid="67" duration="100"/>
+   <frame tileid="68" duration="100"/>
+   <frame tileid="69" duration="100"/>
+   <frame tileid="70" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="75">
+  <animation>
+   <frame tileid="75" duration="100"/>
+   <frame tileid="76" duration="100"/>
+   <frame tileid="77" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="79">
+  <animation>
+   <frame tileid="79" duration="100"/>
+   <frame tileid="80" duration="100"/>
+   <frame tileid="81" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="84">
+  <animation>
+   <frame tileid="84" duration="100"/>
+   <frame tileid="85" duration="100"/>
+   <frame tileid="86" duration="100"/>
+   <frame tileid="87" duration="100"/>
+   <frame tileid="88" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="89">
+  <animation>
+   <frame tileid="89" duration="100"/>
+   <frame tileid="90" duration="100"/>
+   <frame tileid="91" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="96">
+  <animation>
+   <frame tileid="96" duration="100"/>
+   <frame tileid="97" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="697">
+  <animation>
+   <frame tileid="683" duration="100"/>
+   <frame tileid="684" duration="100"/>
+   <frame tileid="685" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="708">
+  <animation>
+   <frame tileid="708" duration="100"/>
+   <frame tileid="709" duration="100"/>
+   <frame tileid="710" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="1359">
+  <animation>
+   <frame tileid="1359" duration="100"/>
+   <frame tileid="1360" duration="100"/>
+   <frame tileid="1361" duration="100"/>
+   <frame tileid="1362" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="1493">
+  <animation>
+   <frame tileid="1493" duration="100"/>
+   <frame tileid="1494" duration="100"/>
+   <frame tileid="1495" duration="100"/>
+  </animation>
+ </tile>
 </tileset>
-
