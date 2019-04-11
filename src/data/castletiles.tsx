fd4e295b66d581d58151c0b51ea172d88d1ff200
alex-worker <43813540+alex-worker@users.noreply.github.com>
2019-04-11 08:57:58 +0300

    some tile

---
+++ src/data/castletiles.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="castletiles" tilewidth="32" tileheight="32" tilecount="24320" columns="64">
+ <image source="tiles.png" trans="ff00ff" width="2048" height="3040"/>
+</tileset>
