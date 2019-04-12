e2ae5aed8cd926d4a34a4e7c66ac3a08b06c0cb2
alex-worker <43813540+alex-worker@users.noreply.github.com>
2019-04-12 10:53:24 +0300

    читаем тайлы из отдельного файла либо из этого же

--- src/main.go
+++ src/main.go
@@ -7,8 +7,8 @@ import (
 )
 
 const fontName string = "CaslonBold"
-// const mapName string = "swamp.tmx"
-const mapName string = "laboratory3.tmx"
+const mapName string = "swamp.tmx"
+// const mapName string = "laboratory3.tmx"
 const resPath string = "data/"
 
 var screenSize = def.Rect{ 
