e5f498a474a5c2108f94211da0fd6de9f95895e7
alex-worker <alex-worker@email>
2019-04-14 06:53:47 +0300

    try...

--- src/engine/engine.go
+++ src/engine/engine.go
@@ -50,36 +50,55 @@ func Init(info def.LoadInfo){
 
 }
 
+func updateGame() bool{
+
+	evt := ui.GetInput()
+
+	if evt == def.EventQuit {
+		return false
+	}
+
+	switch evt {
+	case def.EventPressDown:
+		entity.HeroDo( def.DirDown, def.ActionStand )
+	case def.EventPressUp:
+		entity.HeroDo( def.DirUp, def.ActionStand )
+	case def.EventPressLeft:
+		entity.HeroDo( def.DirLeft, def.ActionStand )
+	case def.EventPressRight:
+		entity.HeroDo( def.DirRight, def.ActionStand )
+	}
+	return true
+}
+
+func drawGame(){
+
+	mymap, w, h := entity.GetMap()
+	ui.LookAtHero( mymap, int(w), int(h), &hero )
+
+}
+
 // Run цикл
 func Run(){
 
 	fmt.Println("Engine run...")
 
-	var evt def.GameEvent
-
 	for true {
 
-		evt = ui.GetInput()
-
-		if evt == def.EventQuit {
+		// start := ui.GetTickCount()
+		if !updateGame(){
 			break
 		}
+		drawGame()
 
-		switch evt {
-		case def.EventPressDown:
-			entity.HeroDo( def.DirDown, def.ActionStand )
-		case def.EventPressUp:
-			entity.HeroDo( def.DirUp, def.ActionStand )
-		case def.EventPressLeft:
-			entity.HeroDo( def.DirLeft, def.ActionStand )
-		case def.EventPressRight:
-			entity.HeroDo( def.DirRight, def.ActionStand )
-		}
-
-		mymap, w, h := entity.GetMap()
-		
-		ui.LookAtHero( mymap, int(w), int(h), &hero )
-
+		// time := int(ui.GetTickCount()) - int(start);
+		// // if (time < 0) continue; // if time is negative, the time probably overflew, so continue asap
+		// if time>0 {
+		// 	sleepTime := 1000/10 - time
+		// 	if sleepTime > 0 {
+		// 		// ui.Delay( uint32(sleepTime))
+		// 	}	
+		// }
 	}
 
 	ui.Destroy()
