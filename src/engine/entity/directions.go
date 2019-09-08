3742383599cab0f027a4cc611c1470fc3f70073d
sir Random <no@mail.me>
2019-09-08 20:24:28 +0300

    I'm happy user of GoLand IDE

--- src/engine/entity/directions.go
+++ src/engine/entity/directions.go
@@ -5,38 +5,38 @@ import (
 	"errors"
 )
 
-func calcNewPos( pos *def.Pos, dir def.Direction, maxPosX uint32, maxPosY uint32 )( *def.Pos, error){
+func calcNewPos(pos *def.Pos, dir def.Direction, maxPosX uint32, maxPosY uint32) (*def.Pos, error) {
 
 	x := pos.X
 	y := pos.Y
 
-	if ( dir == def.DirLeft ){
+	if dir == def.DirLeft {
 		x--
-	} else if ( dir == def.DirRight ){
+	} else if dir == def.DirRight {
 		x++
-	} else if ( dir == def.DirUp ){
+	} else if dir == def.DirUp {
 		y--
-	} else if ( dir == def.DirDown ){
+	} else if dir == def.DirDown {
 		y++
 	}
 
-	if ( x<0 ) {
-		return nil, errors.New("Туда нельзя")
+	if x < 0 {
+		return nil, errors.New("туда нельзя")
 	}
 
-	if ( y<0 ) {
-		return nil, errors.New("Туда нельзя")
+	if y < 0 {
+		return nil, errors.New("туда нельзя")
 	}
 
-	if ( x >= maxPosX ){
-		return nil, errors.New("Туда нельзя")
+	if x >= maxPosX {
+		return nil, errors.New("туда нельзя")
 	}
 
-	if ( y >= maxPosY ){
-		return nil, errors.New("Туда нельзя")
+	if y >= maxPosY {
+		return nil, errors.New("туда нельзя")
 	}
 
-	newPos := def.Pos{ X: x, Y: y}
+	newPos := def.Pos{X: x, Y: y}
 	return &newPos, nil
 
 }
