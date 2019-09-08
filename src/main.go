afb98daa8b123a8075f08ea7703e8945559f1a90
alex-worker <alex-worker@email>
2019-09-08 20:49:02 +0300

    so more happy using of GoLand IDE

--- src/main.go
+++ src/main.go
@@ -1,37 +1,38 @@
 package main
 
 import (
-    "flag"
-    "fmt"
-    "./engine"
-    "./engine/def"
+	"./engine"
+	"./engine/def"
+	"flag"
+	"fmt"
 )
 
 const fontName string = "CaslonBold"
 const mapName string = "swamp.tmx"
+
 // const mapName string = "laboratory3.tmx"
 // const mapName string = "mycastle.tmx"
 const resPath string = "data/"
 
 var screenSize = def.Size{
-    Width: 800,
-    Height: 600,
+	Width:  800,
+	Height: 600,
 }
 
-func main(){
+func main() {
 
-    resDir := resPath
+	resDir := resPath
 
-    flag.StringVar(&resDir, "dir", resPath, "directory path")
-    flag.Parse()
+	flag.StringVar(&resDir, "dir", resPath, "directory path")
+	flag.Parse()
 
-    loadInfo := def.LoadInfo{ 
-        MapName: mapName,
-        FontName: fontName,
-        ResourceFolder: resDir,
-        ScreenSize: screenSize,
-    }
-    fmt.Println("Hello!")
-    engine.Init(loadInfo)
-    engine.Run()
+	loadInfo := def.LoadInfo{
+		MapName:        mapName,
+		FontName:       fontName,
+		ResourceFolder: resDir,
+		ScreenSize:     screenSize,
+	}
+	fmt.Println("Hello!")
+	engine.Init(loadInfo)
+	engine.Run()
 }
