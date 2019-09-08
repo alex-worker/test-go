9179b8df031900454b1ac76b234a141b7cfc56dd
alex-worker <alex-worker@email>
2019-09-08 20:35:31 +0300

    I'm happy user of GoLand IDE again

--- src/engine/ui/imageloader.go
+++ src/engine/ui/imageloader.go
@@ -3,13 +3,13 @@ package ui
 import (
 	"../def"
 	"fmt"
-	"image/png"
 	"github.com/veandco/go-sdl2/sdl"
+	"image/png"
 )
 
-func imgFileToTexture(filename string) (texture *sdl.Texture, w int, h int ) {
+func imgFileToTexture(filename string) (texture *sdl.Texture, w int, h int) {
 	fmt.Println("Load texture...", filename)
-	infile, err := def.OpenFile( filename )
+	infile, err := def.OpenFile(filename)
 	if err != nil {
 		panic(err)
 	}
@@ -22,27 +22,31 @@ func imgFileToTexture(filename string) (texture *sdl.Texture, w int, h int ) {
 	w = img.Bounds().Max.X
 	h = img.Bounds().Max.Y
 
-	pixels := make([]byte,w*h*4)
+	pixels := make([]byte, w*h*4)
 	bIndex := 0
-	for y:=0; y < h; y++ {
-		for x:=0; x < w; x++ {
+	for y := 0; y < h; y++ {
+		for x := 0; x < w; x++ {
 			r, g, b, a := img.At(x, y).RGBA()
-			pixels[bIndex] = byte(r / 256 )
+			pixels[bIndex] = byte(r / 256)
 			bIndex++
-			pixels[bIndex] = byte(g / 256 )
+			pixels[bIndex] = byte(g / 256)
 			bIndex++
-			pixels[bIndex] = byte(b / 256 )
+			pixels[bIndex] = byte(b / 256)
 			bIndex++
-			pixels[bIndex] = byte(a / 256 )
+			pixels[bIndex] = byte(a / 256)
 			bIndex++
 		}
 	}
 
-	texture,err = renderer.CreateTexture(sdl.PIXELFORMAT_ABGR8888, sdl.TEXTUREACCESS_STATIC, int32(w), int32(h) )
+	texture, err = renderer.CreateTexture(sdl.PIXELFORMAT_ABGR8888, sdl.TEXTUREACCESS_STATIC, int32(w), int32(h))
 	if err != nil {
 		panic(err)
 	}
-	texture.Update(nil, pixels, w*4)
+
+	err = texture.Update(nil, pixels, w*4)
+	if err != nil {
+		panic(err)
+	}
 
 	err = texture.SetBlendMode(sdl.BLENDMODE_BLEND)
 	if err != nil {
