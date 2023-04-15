d607785802c784a3c5d3a8c7ec364517df6ae04e
alex-worker <alex-worker@email>
2023-04-15 14:19:05 +0300

    refs

--- src/core/ResourceManager/ResourceManager_test.go
+++ src/core/ResourceManager/ResourceManager_test.go
@@ -1,25 +1,36 @@
 package ResourceManager
 
 import (
+	"path/filepath"
+	"runtime"
 	"testing"
 )
 import "github.com/stretchr/testify/assert"
 
-func TestResourceManager(t *testing.T) {
-	myManager := getResourceManager("/data")
+func getCurrentDir(t *testing.T) string {
+	_, filename, _, _ := runtime.Caller(0)
+	dir := filepath.Dir(filename)
+	return dir
+}
+
+func TestResourcesManagerForEmpty(t *testing.T) {
+	myManager := getResourceManager("../../../data")
 	res, err := myManager.GetResource("not existing path")
-
 	assert.Equal(t, res, nil, "Must be equal")
-	assert.Equal(t, err.Error(), "not found", "Must be equal")
+	assert.NotEqualf(t, err, nil, "Must be equal")
+}
 
-	res, err = myManager.GetResource("castletiles.tsx")
+func TestResourcesManager(t *testing.T) {
+	myManager := getResourceManager("../../../data")
+
+	res, err := myManager.GetResource("castletiles.tsx")
 	assert.Equal(t, err, nil, "Must be equal")
 
 	percent := res.GetReadyPercent()
 	assert.Equal(t, percent, uint8(0), "Must be equal")
 
 	state := res.GetState()
-	assert.Equal(t, state, Closed, "Must be equal")
+	assert.Equal(t, state, Waiting, "Must be equal")
 
 	_, err = res.GetContent()
 	assert.Equal(t, err.Error(), "not ready", "Must be equal")
