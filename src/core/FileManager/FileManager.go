20a763abc8e8c65b11412961844303180f066fbb
sir Random <RandomAmbersky@gmail.com>
2023-04-16 09:24:19 +0300

    refs

--- src/core/FileManager/FileManager.go
+++ src/core/FileManager/FileManager.go
@@ -1,23 +1,32 @@
 package FileManager
 
 import (
+	"fmt"
 	"os"
 	"path/filepath"
 	"test-go/src/core/IResourceManager"
 )
 
 type FileManager struct {
-	resFolder string
+	resFolder    string
+	resourceList map[string]IResourceManager.IResource
 }
 
-func (r FileManager) GetResource(path string) (IResourceManager.IResource, error) {
+func (r *FileManager) Release() {
+	for k, res := range r.resourceList {
+		fmt.Println(k)
+		res.Release()
+	}
+}
+
+func (r *FileManager) GetResource(path string) (IResourceManager.IResource, error) {
 	filePath := filepath.Join(r.resFolder, path)
 	_, err := os.Stat(filePath)
 	if err != nil {
 		return nil, err
 	}
 
-	res := FileResource{
+	res := &FileResource{
 		state: IResourceManager.InternalResourceState{
 			FilePath:     filePath,
 			State:        IResourceManager.Waiting,
@@ -25,11 +34,13 @@ func (r FileManager) GetResource(path string) (IResourceManager.IResource, error
 		},
 		file: nil,
 	}
-	return &res, nil
+	r.resourceList[filePath] = res
+	return res, nil
 }
 
 func getFileManager(dir string) IResourceManager.IResourceManager {
-	return FileManager{
-		resFolder: dir,
+	return &FileManager{
+		resFolder:    dir,
+		resourceList: make(map[string]IResourceManager.IResource),
 	}
 }
--- src/core/IResourceManager/IResourceManager.go
+++ src/core/IResourceManager/IResourceManager.go
@@ -2,4 +2,5 @@ package IResourceManager
 
 type IResourceManager interface {
 	GetResource(path string) (IResource, error)
+	Release()
 }
