9e2f4e7f9a43c88bc3a7a402edfdbf342b56584a
sir Random <RandomAmbersky@gmail.com>
2023-04-16 09:42:53 +0300

    refs

--- src/core/Engine/Engine.go
+++ src/core/Engine/Engine.go
@@ -1,20 +1,20 @@
 package Engine
 
 import (
-	"test-go/src/core/FileManager"
+	. "test-go/src/core/FileManager"
 	"test-go/src/core/IEngine"
-	"test-go/src/core/IResourceManager"
+	. "test-go/src/core/IResourceManager"
 )
 
 type Engine struct {
-	resourceManager IResourceManager.IResourceManager
+	resourceManager IResourceManager
 }
 
 func (e *Engine) Run() {
 }
 
 func GetEngine() IEngine.IEngine {
-	resourceManager := FileManager.GetFileManager("/data")
+	resourceManager := GetFileManager("/data")
 	eng := &Engine{
 		resourceManager: resourceManager,
 	}
