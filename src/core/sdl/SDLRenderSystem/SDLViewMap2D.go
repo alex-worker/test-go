198431b0abb5e437b498dd7d3214e33e524e8c0d
alex-worker <alex-worker@no-mail.com>
2023-05-17 10:21:03 +0300

    update

--- src/core/sdl/SDLRenderSystem/SDLViewMap2D.go
+++ src/core/sdl/SDLRenderSystem/SDLViewMap2D.go
@@ -1,7 +1,17 @@
 package SDLRenderSystem
 
+import (
+	. "test-go/src/defines"
+)
+
 type SDLViewMap2D struct{}
 
-func (v *SDLViewMap2D) GetCell() {
+func (v *SDLViewMap2D) GetCell(p Point2D) Cell {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (v *SDLViewMap2D) SetCell(p Point2D, c Cell) error {
+	//TODO implement me
 	panic("implement me")
 }
--- src/interfaces/IRenderSystem/IViewMap2D.go
+++ src/interfaces/IRenderSystem/IViewMap2D.go
@@ -1,5 +1,10 @@
 package IRenderSystem
 
+import (
+	. "test-go/src/defines"
+)
+
 type IViewMap2D interface {
-	GetCell()
+	GetCell(p Point2D) Cell
+	SetCell(p Point2D, c Cell) error
 }
