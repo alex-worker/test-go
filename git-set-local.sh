dfd9bbfe0ba928f7f580af60c0cfcb1fcacfb87f
alex-worker <43813540+alex-worker@users.noreply.github.com>
2024-01-05 14:09:54 +0300

    Delete git-set-local.sh

--- git-set-local.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-git config --local user.name "alex-worker"
-git config --local user.email "alex-worker@no-mail.com"
-git config --local core.sshCommand 'ssh -i ~/.ssh/ed_alex-worker -o IdentityAgent=none'
